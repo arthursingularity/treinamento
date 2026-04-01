3d78e60eb9dd56e641a34ba2ffb56e8ecb59c294
Arthur <arthur.1234.5678.alves@gmail.com>
2026-04-01 20:44:24 -0300

    update!

--- src/app/page.js
+++ src/app/page.js
@@ -348,7 +348,7 @@ export default function Home() {
                 <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z" />
                 <circle cx="12" cy="10" r="3" />
               </svg>
-              <span>Localiza</span>
+              <span>Localizar</span>
             </button>
 
             <button
